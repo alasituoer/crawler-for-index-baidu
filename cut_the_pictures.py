e276a70f358800db789dd35ef85d950d5cac40c8
alasituoer <mingzhi_6143@163.com>
2017-04-20 19:27:06 +0800

    add 1 file

---
+++ cut_the_pictures.py
@@ -0,0 +1,25 @@
+#coding:utf-8
+from PIL import Image
+
+working_space = '/Users/Alas/alas/python/selenium/captures/'
+
+im = Image.open(working_space + '1号店.png')
+print im.format, im.size, im.mode
+#im.show()
+
+index_profile = im.crop((36, 476, 2231, 754))
+#index_profile.show()
+
+start_date = index_profile.crop((235, 25, 375, 55))
+end_date = index_profile.crop((400, 25, 535, 55))
+#start_date.show()
+#end_date.show()
+
+search_index_all = index_profile.crop((536, 185, 715, 250))
+search_index_mobile = index_profile.crop((715, 185, 900, 250))
+#search_index_all.show()
+#search_index_mobile.show()
+
+keyword = index_profile.crop((45, 175, 325, 255))
+#keyword.show()
+
