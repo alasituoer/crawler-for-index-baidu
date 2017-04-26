88a4e9806d8dbc97857a0e40199c107a8a8b4f99
alasituoer <mingzhi_6143@163.com>
2017-04-26 11:29:01 +0800

    move the pictures to the new folder cropped_files

--- cut_the_pictures.py
+++ cut_the_pictures.py
@@ -1,25 +1,35 @@
 #coding:utf-8
 from PIL import Image
 
-working_space = 'captures/'
+working_space = 'captures/source_files/'
 
 im = Image.open(working_space + '1号店.png')
 print im.format, im.size, im.mode
 #im.show()
 
 index_profile = im.crop((36, 476, 2231, 754))
+index_profile.save('captures/cropped_files/' + 'index_profile.png', 'png')
 #index_profile.show()
 
 start_date = index_profile.crop((235, 25, 375, 55))
 end_date = index_profile.crop((400, 25, 535, 55))
+start_date.save('captures/cropped_files/' + 'start_date.png', 'png')
+end_date.save('captures/cropped_files/' + 'end_date.png', 'png')
+#index_profile.show()
 #start_date.show()
 #end_date.show()
 
 search_index_all = index_profile.crop((536, 185, 715, 250))
 search_index_mobile = index_profile.crop((715, 185, 900, 250))
+search_index_all.save('captures/cropped_files/' + 'search_index_all.png', 'png')
+search_index_mobile.save('captures/cropped_files/' + 'search_index_mobile.png', 'png')
 #search_index_all.show()
 #search_index_mobile.show()
 
 keyword = index_profile.crop((45, 175, 325, 255))
+keyword.save('captures/cropped_files/' + 'keyword.png', 'png')
 #keyword.show()
+
+
+
 
